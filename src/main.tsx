a30b980322f4d2c93486e5f20e8fbee4ff24ca96
musharaf <mk.guvi123@gmail.com>
2023-11-04 09:43:50 +0530

    [feature] added reactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
